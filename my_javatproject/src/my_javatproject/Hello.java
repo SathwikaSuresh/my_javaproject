0cddfc4e07743cb349675ad8ec61f92d51f05593
sathwika <sathwika@DESKTOP-1ODT322>
2024-06-20 20:25:24 +0530

    Initial Commit

---
+++ my_javatproject/src/my_javatproject/Hello.java
@@ -0,0 +1,10 @@
+package my_javatproject;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("HELLO WORLD");
+	}     
+	
+}
